a31382e59454a8df9fe9e96263b04c7266f4c2ce
Leigh Simpson <code@simpleigh.com>
2021-01-15 21:12:55 +0000

    Replaced sample addon with N-API implementation
    ### Why?
    
    > N-API is an API for building native addons. It is independent from the
    > underlying JavaScript runtime (e.g. V8) and is maintained as part of
    > Node.js itself.
    
    (https://nodejs.org/docs/latest-v14.x/api/addons.html#addons_n_api)
    
    This should mean we can avoid having to wrangle with v8.
    
    ### What?
    
    * replace the `hello.cpp` sample with the version at the above link

--- src/hello.cpp
+++ src/hello.cpp
@@ -1,24 +1,28 @@
-#include <node.h>
+#include <node_api.h>
 
 namespace demo {
 
-using v8::FunctionCallbackInfo;
-using v8::Isolate;
-using v8::Local;
-using v8::Object;
-using v8::String;
-using v8::Value;
+napi_value Method(napi_env env, napi_callback_info args) {
+  napi_value greeting;
+  napi_status status;
 
-void Method(const FunctionCallbackInfo<Value>& args) {
-  Isolate* isolate = args.GetIsolate();
-  args.GetReturnValue().Set(String::NewFromUtf8(
-      isolate, "world").ToLocalChecked());
+  status = napi_create_string_utf8(env, "world", NAPI_AUTO_LENGTH, &greeting);
+  if (status != napi_ok) return nullptr;
+  return greeting;
 }
 
-void Initialize(Local<Object> exports) {
-  NODE_SET_METHOD(exports, "hello", Method);
+napi_value init(napi_env env, napi_value exports) {
+  napi_status status;
+  napi_value fn;
+
+  status = napi_create_function(env, nullptr, 0, Method, nullptr, &fn);
+  if (status != napi_ok) return nullptr;
+
+  status = napi_set_named_property(env, exports, "hello", fn);
+  if (status != napi_ok) return nullptr;
+  return exports;
 }
 
-NODE_MODULE(NODE_GYP_MODULE_NAME, Initialize)
+NAPI_MODULE(NODE_GYP_MODULE_NAME, init)
 
 }  // namespace demo
